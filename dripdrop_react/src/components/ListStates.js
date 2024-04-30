0a378dd6153f796192589dcb91a6eec08dcf507d
Marcelo Matos <aluno23028@ipt.pt>
2024-04-30 13:36:57 +0100

    liststates fix

---
+++ dripdrop_react/src/components/ListStates.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+import { useNavigate} from "react-router-dom";
+
+export default function ListStates(){
+    return(
+        <div>
+        </div>  
+    )
+}
